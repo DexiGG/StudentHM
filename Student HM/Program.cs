b0956bcb9bee4adb65f919a54eebf61cecb777dd
unknown <invokedex@gmail.com>
2019-12-09 03:11:13 -0700

    StudentHM

---
+++ Student HM/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_HM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<Student>();
+
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine(
+                "1) Добавить студента\n" +
+                "2) Список студентов\n" +
+                "3) Отобразить лучшего студента\n" +
+                "4) Отобразить худшего студента\n" +
+                "5) Отобразить среднюю оценку\n" +
+                "6) Выход из приложения");
+
+                int choice = 0;
+                bool success = int.TryParse(Console.ReadLine(), out choice);
+
+                Console.Clear();
+
+
+
+                if (success && (choice > 0 && choice <= 6))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            {
+                                Console.WriteLine("Введите полное имя студента:");
+                                string fullName = Console.ReadLine();
+
+                                float gpa;
+                                do
+                                {
+                                    Console.Clear();
+                                    Console.Write("Введите среднюю оценку студента: ");
+                                } while (!float.TryParse(Console.ReadLine(), out gpa));
+
+                                list.Add(new Student { FullName = fullName, GPA = gpa });
+                            }
+                            break;
+                        case 2:
+                            {
+                                if (list.Count > 0)
+                                {
+                                    list.Sort(new Student());
+
+                                    foreach (Student student in list)
+                                    {
+                                        Console.WriteLine(student);
+                                    }
+                                }
+                            }
+                            break;
+                        case 3:
+                            {
+                                float rank = list.Max(student => student.GPA);
+
+                                List<Student> students = list.FindAll(s => s.GPA == rank);
+
+                                if (students != null)
+                                {
+                                    ShowListOfStudents(students);
+                                }
+                            }
+                            break;
+                        case 4:
+                            {
+                                float rank = list.Min(student => student.GPA);
+
+                                List<Student> students = list.FindAll(s => s.GPA == rank);
+
+                                if (students != null)
+                                {
+                                    ShowListOfStudents(students);
+                                }
+                            }
+                            break;
+                        case 5:
+                            {
+                                float avgRank = list.Average(student => student.GPA);
+
+                                ShowListOfStudents(list);
+                                Console.WriteLine("\nСредняя оценка студентов: {0}", avgRank);
+                            }
+                            break;
+                        case 6: Environment.Exit(0); break;
+                        default:
+                            break;
+                    }
+                    Console.WriteLine("Чтобы продолжить нажмите Enter...");
+                    Console.ReadLine();
+                }
+            }
+        }
+        static void ShowListOfStudents(List<Student> students)
+        {
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student);
+
+            }
+        }
+    }
+}
---
+++ Student HM/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_HM
+{
+    public class Student : IComparer<Student>
+    {
+        public string FullName { get; set; }
+        public float GPA { get; set; }
+
+        public int Compare(Student first, Student second)
+        {
+            return string.Compare(first.FullName, second.FullName);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Имя: {0}, Оценка: {1}", FullName, GPA);
+        }
+    }
+}
